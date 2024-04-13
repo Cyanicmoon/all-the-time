5c85b79200fd54f293d5d6b4158ab7c81de260ab
cyanicmoon <jihyohu@gmail.com>
2024-04-14 03:52:22 +0900

    up

--- app/page.js
+++ app/page.js
@@ -7,6 +7,9 @@ export const revalidate = 60; // 첫 방문시 캐싱
 export default async function Home() {
 
   return (
-    <div>test1234</div>
+    // <div>test1234</div>
+    <div>
+      <img src="/elmo.gif" className="test-img"></img>
+    </div>
   );
 }
