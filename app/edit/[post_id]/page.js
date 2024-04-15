02647ca8c4c881c8ce650a1a8d26ff78816a66e5
cyanicmoon <jihyohu@gmail.com>
2024-04-16 08:20:12 +0900

    edit fixed

--- app/edit/[post_id]/page.js
+++ app/edit/[post_id]/page.js
@@ -15,24 +15,40 @@ export default async function Detail(props){
     if (session.user.email != post.author){
         return <div>잘못된 접근입니다</div>
     }
-    
-    return(
-        <div>
-            <div className="post-container" style={{paddingBottom:"10px"}}>
-                <div className="post-container-main" style={{border:"0", marginBottom:"0"}}>
-                    <div className="post-container-header" >
-                            <h2 style={{marginTop:"10px", fontSize:"30px"}}>게시글 수정</h2>
-                    </div>
 
-                    <div className="add-container-content">
-                        <form action="/api/post/add" method="POST">
-                            <input type="text" name="title" placeholder="제목을 입력하세요" defaultValue={post.title}></input>
-                            <textarea name="content" placeholder="내용을 입력하세요" defaultValue={post.content}></textarea>
-                            <button type="submit">✏️ 수정</button>
-                        </form>
-                    </div>
+    // await db.collection("post").updateOne({ _id : new ObjectId(props.params.post_id) }, 
+    //     {$set : { title : '', content : '' }}
+    // )
+
+    // console.log(post)
+    return(
+        // <div className="p-20">
+        //     <h4>글 수정</h4>
+        //     <form action="/api/post/edit" method="POST">
+        //         <input name="title" placeholder="제목을 입력하세요" defaultValue={post.title}></input>
+        //         <textarea name="content" placeholder="내용을 입력하세요" defaultValue={post.content}></textarea>
+        //         <input style={{display: "none"}}name="_id" defaultValue={post._id.toString()}></input>
+        //         <button type="submit">작성</button>
+        //     </form>
+        // </div>
+
+        <div>
+        <div className="post-container" style={{paddingBottom:"10px"}}>
+            <div className="post-container-main" style={{border:"0", marginBottom:"0"}}>
+                <div className="post-container-header" >
+                        <h2 style={{marginTop:"10px", fontSize:"30px"}}>게시글 수정</h2>
+                </div>
+
+                <div className="add-container-content">
+                    <form action="/api/post/edit" method="POST">
+                        <input type="text" name="title" placeholder="제목을 입력하세요" defaultValue={post.title}></input>
+                        <textarea name="content" placeholder="내용을 입력하세요" defaultValue={post.content}></textarea>
+                        <input style={{display: "none"}}name="_id" defaultValue={post._id.toString()}></input>
+                        <button type="submit">✏️ 수정</button>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
     )
 }
