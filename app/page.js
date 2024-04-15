8507863507dfaff932d6a84d1ec70ef6e9b88726
cyanicmoon <jihyohu@gmail.com>
2024-04-15 15:07:35 +0900

    main page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
-import { connectDB } from "@/util/database";
-import { MongoClient } from "mongodb";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
 
 
 export const revalidate = 60; // 첫 방문시 캐싱
 
 export default async function Home() {
 
+  let session = await getServerSession(authOptions);
+
   return (
-    // <div>test1234</div>
     <div>
-      <img src="/elmo.gif" className="test-img"></img>
+      {
+        session ?  <img src="/elmo_yes.gif" className="test-img"></img> : <img src="/elmo_no.gif" className="test-img"></img>
+      }
     </div>
   );
 }
